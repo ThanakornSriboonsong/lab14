eaaa9569609febb4151cbe7b176986d1eaded1d7
ThanakornSriboonsong <plemplemplemplem5@gmail.com>
2025-01-28 16:32:59 +0700

    Update lab14_2.cpp

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -20,4 +20,39 @@ int main(){
 	return 0;
 }
 
-//Do not modify source code above this line 
+//Do not modify source code above this line
+void inputMatrix(double A[][N] ){
+   
+    for(int i = 0 ; i < N ; i++ ){
+        cout << "Row " << i+1 << ": " ;
+        for(int j = 0 ; j < N ; j++){
+            cin >> A[i][j] ;
+        }
+    }
+}
+     
+void showMatrix(const bool A[][N]){
+    for(int i = 0 ; i < N ;i++){
+        for(int j = 0 ; j < N ; j++){
+          cout << A[i][j] ;
+           cout << " " ;
+        }
+        cout << endl ;
+    }
+}
+void findLocalMax(const double A[][N], bool B[][N]){
+    for(int i = 0 ; i < N ; i++){
+        for(int j = 0 ; j < N ; j++){
+            
+            if( i <= N-2 and j <= N-2){
+                if(A[i][j] < A[i-1][j] or A[i][j] < A[i][j-1] or A[i][j] < A[i][j+1] or A[i][j] < A[i+1][j]){
+                    B[i][j] = 0 ;
+                }else{
+                    B[i][j] = 1 ;
+                }
+            }
+                if(i == 0 or j == 0 or j == N-1)B[i][j] = 0 ;                   
+        }
+    }
+    
+}
